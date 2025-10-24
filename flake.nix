5ebaf47a00ad12cdff878ac082882a5e52bcddd6
mystreamer <me@dylanmassey.ch>
2025-10-25 01:55:02 +0200

    commit flake

--- flake.nix
+++ flake.nix
@@ -4,40 +4,42 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    nixosModules.default = { pkgs, config, ... }: {
+    nixosModules.default = { config, pkgs, ... }: {
+      boot.isContainer = true;
+      networking.useDHCP = false;
+      networking.firewall.enable = false;
+      system.stateVersion = "24.05";
+
       environment.systemPackages = with pkgs; [
-        (python3.withPackages (ps: [ 
-          ps.openai
-          ps.pikepdf
-          ps.pdf2image
-          ps.outlines
-          ps.pillow
-          ps.bottle
+        (python3.withPackages (ps: with ps; [ 
+          bottle
+          openai
+          pikepdf
+          pdf2image
+          pillow
         ]))
         curl
         jq
       ];
-      
+
+      environment.etc."quicktoc/app.py".text = ''
+        from bottle import route, run
+        
+        @route('/')
+        def hello():
+            return "Hello from QuickTOC!"
+        
+        run(host='0.0.0.0', port=8080)
+      '';
+
       systemd.services.quicktoc = {
         enable = true;
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
-          ExecStart = "${pkgs.python3.withPackages (ps: [ ps.bottle ])}/bin/python app.py";
-          WorkingDirectory = "/app";
+          ExecStart = "${pkgs.python3.withPackages (ps: [ ps.bottle ])}/bin/python /etc/quicktoc/app.py";
           Restart = "always";
         };
       };
-      
-      system.activationScripts.copyApp = ''
-        mkdir -p /app
-        cp -r ${self}/* /app/
-      '';
-    };
-
-    # Test container configuration
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [ self.nixosModules.default ];
     };
   };
 }
